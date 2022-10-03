84b8392c094d29360524366403c1bf5b4a4f032f
Артем <chavikina1988@gmail.com>
2022-10-03 16:26:51 +0300

    03.10

--- 6. задание 5 способ 2.py
+++ 6. задание 5 способ 2.py
@@ -1,10 +1,11 @@
 import random
-spisok =[]# создаем путстой список
-for i in range(10): #создаем цикл по 10 элементам
-    spisok.append(random.randint(0,30)) # создаем список из случайных значений в диапозоне от 0 до 30
-spisok_res = [x for x in spisok if x > 5 ] # запустил генератор списков
+
+spisok = []  # создаем путстой список
+for i in range(10):  # создаем цикл по 10 элементам
+    spisok.append(random.randint(0, 30))  # создаем список из случайных значений в диапозоне от 0 до 30
+spisok_res = [x for x in spisok if x > 5]  # запустил генератор списков
 print(spisok_res)
-print(sum(spisok_res) / len(spisok_res)) # выводим средее арифм. фун лен- длина списка, сум - сумма эл.
+print(sum(spisok_res) / len(spisok_res))  # выводим средее арифм. фун лен- длина списка, сум - сумма эл.
 
 '''import random
 spisok =[]
