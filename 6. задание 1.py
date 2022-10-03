d249ac7c7d371fbfbf2ddea716cb2c956e179af7
Артем <chavikina1988@gmail.com>
2022-10-03 15:50:59 +0300

    03.10

--- 6. задание 1.py
+++
@@ -1,9 +0,0 @@
-a = input()
-s = list(a)
-for i in range(s.count('a')):
-    s.remove('a')
-for i in range(s.count('e')):
-    s.remove('e')
-for i in range(s.count('u')):
-    s.remove('u')
-print(s)
